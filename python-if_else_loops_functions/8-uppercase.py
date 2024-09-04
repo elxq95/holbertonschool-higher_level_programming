0b5d0aca87de6365f0fe0b76a8865241ca1cc3e6
elxq95 <leongxiqern12@gmail.com>
2024-09-04 16:39:53 +1000

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase_str = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            uppercase+str += chr(ord(char)-32)
+        else:
+            uppercase_str += char
+            print("{}".format(uppercase_str))

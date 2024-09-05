b0030b2a38c28f1aa4f9da33dc70f56001614783
elxq95 <leongxiqern12@gmail.com>
2024-09-05 16:26:39 +1000

    push

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
-        print("{1 argument:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(argc))
+
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))

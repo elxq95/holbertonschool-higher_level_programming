3b11f8ca80919526b0b4ee97766a31efea8280de
elxq95 <leongxiqern12@gmail.com>
2024-09-09 12:55:27 +1000

    push

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
 
-    if idx < 0:
-        return my_list
-    if idx > element:
+    if idx < 0 or idx >= len(my_list):
         return my_list
 
-    element = new_element
+    my_list[idx] = element
 
-    return my_list[element]
+    return my_list

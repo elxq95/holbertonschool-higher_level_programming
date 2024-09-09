79507ef7593011dee92583fe5e2e4abe4e5db181
elxq95 <leongxiqern12@gmail.com>
2024-09-09 19:38:52 +1000

    push;

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
 
     new_list = my_list[:]
-    if idx < 0 or idx > len(mylist):
+    if idx < 0 or idx >= len(mylist):
         return new_list
 
     new_list[idx] = element

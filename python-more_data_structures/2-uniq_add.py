2c5f6f0670e520cfa26f6282496e2111a66f5c05
elxq95 <leongxiqern12@gmail.com>
2024-09-11 16:38:26 +1000

    test

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,9 +3,9 @@
 def uniq_add(my_list=[]):
     unique_integers = []
     for num in my_list:
-        #for each number in my_list, we check if it is
-        #already in unique_integers. If not, we append it to
-        #unique integers
+        # for each number in my_list, we check if it is
+        # already in unique_integers. If not, we append it to
+        # unique integers
         if num not in unique_integers:
             unique_integers.append(num)
     return sum(unique_integers)

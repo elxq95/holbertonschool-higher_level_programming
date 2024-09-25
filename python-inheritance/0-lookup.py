4cfdcf1e2db652fce91591c92e92fabed7eb1eea
elxq95 <leongxiqern12@gmail.com>
2024-09-25 21:06:45 +1000

    test

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 """
-This module provides a utility function to inspect objects in Python.
+This module provides a utility function 
+to inspect objects in Python.
 
-The `lookup` function can be used to retrieve a list of available attributes and methods
+The `lookup` function can be used to retrieve a list of available 
+attributes and methods
 of an object, providing insight into what functionality is accessible.
 
 Example usage:
@@ -17,16 +19,21 @@ Example usage:
 
 def lookup(obj):
     """
-    Returns the list of available attributes and methods of an object.
+    Returns the list of available attributes and 
+    methods of an object.
 
-    This function provides introspection capabilities by returning all the attributes
-    and methods associated with the given object `obj`. It does not rely on any external
+    This function provides introspection capabilities 
+    by returning all the attributes
+    and methods associated 
+    with the given object `obj`. It does not rely on any external
     modules and utilizes Python's built-in `dir()` function.
 
     Parameters:
-    obj (any): The object whose attributes and methods are to be retrieved.
+    obj (any): The object whose attributes and 
+    methods are to be retrieved.
 
     Returns:
-    list: A list containing the names of attributes and methods available for `obj`.
+    list: A list containing the names of attributes
+    and methods available for `obj`.
     """
     return dir(obj)

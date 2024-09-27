fcd4a333fc67232a3db7441bde7b9ce9903217be
elxq95 <leongxiqern12@gmail.com>
2024-09-27 17:04:42 +1000

    test

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module provides a class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    function that calculates area
+
+    Parameters:
+    self: the size
+
+    Returns:
+    None
+    """
+    def area(self):
+        raise Exception("area() is not implemented")

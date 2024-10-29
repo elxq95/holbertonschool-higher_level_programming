78a609bdbbfedf9bf06560510137fb59956b89f4
elxq95 <leongxiqern12@gmail.com>
2024-10-29 23:40:52 +1100

    test

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Rectangle class.
+The Rectangle class is currently a placeholder with no attributes or methods.
+"""
+
+
+class Rectangle:
+    """An empty class that defines a rectangle."""
+    pass

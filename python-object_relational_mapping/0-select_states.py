c33a63f1246ab6e69bef3ce62335c5e6c93987b7
elxq95 <leongxiqern12@gmail.com>
2024-11-03 23:41:27 +1100

    test

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Retrieve command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to the MySQL database
+    db_connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor to execute SQL queries
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    
+    # Fetch and print each row
+    for row in cursor.fetchall():
+        print(row)
+
+    # Close cursor and connection
+    cursor.close()
+    db_connection.close()
+    

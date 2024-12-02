b09abf11e5549246be5b65299715989360512569
elxq95 <leongxiqern12@gmail.com>
2024-12-02 16:49:26 +1100

    test

---
+++ python-everything_is_object/0-main.py
@@ -0,0 +1,15 @@
+# Example of using type() function
+
+# Define variables of different types
+integer_var = 42
+float_var = 3.14
+string_var = "Hello, World!"
+list_var = [1, 2, 3]
+dict_var = {"key": "value"}
+
+# Use type() to determine the type of each variable
+print(type(integer_var))  # Output: <class 'int'>
+print(type(float_var))    # Output: <class 'float'>
+print(type(string_var))   # Output: <class 'str'>
+print(type(list_var))     # Output: <class 'list'>
+print(type(dict_var))     # Output: <class 'dict'>
---
+++ python-everything_is_object/1-main.py
@@ -0,0 +1,7 @@
+# Example code
+x = 42
+y = [1, 2, 3]
+
+# Print the memory address (identifier) of the variables
+print("Memory address of x:", id(x)) # Memory address of x: 140710081952272
+print("Memory address of y:", id(y)) # Memory address of y: 2305843009213693952
